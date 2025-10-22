d2c55228ebd13b9b92c1b02a19b0c2e480842f76
Aryan Singh <aryansinghthree@gmail.com>
2025-10-22 17:32:24 +0530

    Updated calculator layout and fixed responsiveness

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -2,7 +2,7 @@ import styles from "./componentcss.module.css";
 const Inputs = ({aryan})=>{
    return(
     <div class="row">
-            <div class="col-sm-8">
+            <div class="col-sm-8" id={styles.inputdiv}>
               <input type="text" id={styles.input} 
                value={aryan}
                readOnly/>
